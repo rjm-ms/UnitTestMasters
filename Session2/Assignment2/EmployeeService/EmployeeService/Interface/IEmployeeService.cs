d01b3f7970cc267dbabc26594ae8875f267d8b04
Romel John Monsanto <rjm.ms@outlook.com>
2023-03-06 13:13:09 +0800

    Add Session 2 project

---
+++ Session2/Assignment2/EmployeeService/EmployeeService/Interface/IEmployeeService.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeService.Interface
+{
+    public interface IEmployeeService
+    {
+        int CreateEmployee(Employee employee);
+
+        IEnumerable<Employee> GetEmployees();
+
+        Employee GetEmployeeById(int id);
+
+        bool EmployeeExist(int id);
+
+
+    }
+}
