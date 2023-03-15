8558e574095f3908778864970eb1627f836128a0
Romel John Monsanto <rjm.ms@outlook.com>
2023-03-15 13:40:04 +0800

    Add Session 4 Assignment

---
+++ Session4/Assignment4/UnitTestMasters_Session4Assignment/ProtectionAgainstRegression.cs
@@ -0,0 +1,33 @@
+﻿namespace UnitTestMasters_Session4Assignment
+{
+    public class ProtectionAgainstRegressionTests
+    {
+        [Fact]
+        public void AddTwoNumbersTest()
+        {
+            // Answer here
+            // Arrange
+            var sut = new Calculator();
+            int num1 = -3, num2 = 7;
+            int expectedOutput = 4;
+
+            // Act
+            int result = sut.Add(num1, num2);
+
+            // Assert
+            Assert.Equal(expectedOutput, result);
+        }
+    }
+
+    internal class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        internal int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
